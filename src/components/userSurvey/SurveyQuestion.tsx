3717b3a8d1a947b4fa7075adeea860390f71616e
aixion1506 <aixion1506@gmail.com>
2024-10-02 16:14:04 +0900

    style: SurveyQuestion padding 16 -> 8

--- src/components/userSurvey/SurveyQuestion.tsx
+++ src/components/userSurvey/SurveyQuestion.tsx
@@ -4,7 +4,7 @@ interface SurveyQuestionProps  {
 
 const SurveyQuestion = ({question}: SurveyQuestionProps ) => {
     return (
-        <div className="px-16 py-16 text-2xl text-center text-white bg-rose-400 rounded-md max-md:px-5 max-md:max-w-full">
+        <div className="px-16 py-8 text-2xl text-center text-white bg-rose-400 rounded-md max-md:px-5 max-md:max-w-full">
             질문: {question}
         </div>
     )
