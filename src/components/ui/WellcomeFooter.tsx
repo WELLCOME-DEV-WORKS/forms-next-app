66cf6eda98b0a0eaa20ea2d93acf42212cc91cb6
nayounghye <yhna.dev@gmail.com>
2024-10-02 14:46:32 +0900

    style: WellcomeFooter

--- src/components/ui/WellcomeFooter.tsx
+++ src/components/ui/WellcomeFooter.tsx
@@ -8,7 +8,7 @@ interface WellcomeFooterProps {
 
 const WellcomeFooter = ({ text }: WellcomeFooterProps) => {
   return (
-    <footer className="sticky bottom-0 flex gap-5 justify-between w-full bg-wellcome-pink whitespace-nowrap  max-md:max-w-full ">
+    <footer className="sticky bottom-0 flex gap-5 justify-between w-full bg-wellcome-pink whitespace-nowrap  max-md:max-w-full p-5">
       <span>{text}</span>
     </footer>
   );
