419c5efbd103660fc38d2c0ca7f972ed5d86d1dc
nayounghye <yhna.dev@gmail.com>
2024-10-04 16:32:03 +0900

    Style: 헤더 폰트 적용

--- src/components/ui/WellcomeHeader.tsx
+++ src/components/ui/WellcomeHeader.tsx
@@ -9,6 +9,13 @@ interface WellcomeHeaderProps {
 const WellcomeHeader = ({ title, buttonText }: WellcomeHeaderProps) => {
   return (
     <header className="flex  gap-5 justify-between w-full bg-wellcome-white text-center whitespace-nowrap  max-md:max-w-full px-8">
+      {/* 로고 폰트 적용 링크 */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+      />
       <Link href="/">
         <div className="flex gap-7 p-2.5 text-xl font-extrabold text-rose-400">
           <img
@@ -17,7 +24,7 @@ const WellcomeHeader = ({ title, buttonText }: WellcomeHeaderProps) => {
             className="object-contain shrink-0 aspect-[1.95] w-[78px]"
             alt="Well-come logo"
           />
-          <div className="my-auto basis-auto">{title}</div>
+          <div className="my-auto basis-auto font-museo ">{title}</div>
         </div>
       </Link>
       <button className="px-4 py-1 text-lg font-bold tracking-tight text-white bg-rose-400 rounded-sm shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:px-3">
