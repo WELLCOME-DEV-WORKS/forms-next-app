01a0f0f4e41cb52e92683febc6de1832b8a7f6d2
nayounghye <yhna.dev@gmail.com>
2024-10-08 16:04:00 +0900

    Style: 푸터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,13 +41,15 @@ export default function RootLayout({
           <WellcomeFooter
             text={
               <>
-                Company Name : Well-come Visa Co., Ltd. Representative : Inhyuk
-                Lee Address : 11th Floor, room 1113 and 1115, 78 Mapo-daero,{' '}
+                Company Name : Well-come Visa Co., Ltd. <br />
+                Representative : Inhyuk Lee <br />
+                Address : 11th Floor, room 1113 and 1115, 78 Mapo-daero,
+                Mapo-gu, Seoul <br />
+                Business Registration Number : 359-86-02092 <br />
+                Email : info@well-come.biz <br />
+                Mail Order Business Registration Number : 2023-Seoul Mapo-3768{' '}
                 <br />
-                Mapo-gu, Seoul Business Registration Number : 359-86-02092 Email{' '}
-                <br />: info@well-come.biz Mail Order Business Registration
-                Number : <br />
-                2023-Seoul Mapo-3768 Customer Service : +82-70-8655-2888"
+                Customer Service : +82-70-8655-2888"
               </>
             }
           />
