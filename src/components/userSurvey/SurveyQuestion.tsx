f567ece0570e93a6b3461f51b744447a7b900e1d
aixion1506 <aixion1506@gmail.com>
2024-10-08 18:01:48 +0900

    Style: 불필요 SurveyQuestion 질문 제거

--- src/components/userSurvey/SurveyQuestion.tsx
+++ src/components/userSurvey/SurveyQuestion.tsx
@@ -4,8 +4,8 @@ interface SurveyQuestionProps  {
 
 const SurveyQuestion = ({question}: SurveyQuestionProps ) => {
     return (
-        <div className="px-16 py-8 text-2xl text-center text-white bg-rose-400 rounded-md max-md:px-5 max-md:max-w-full">
-            질문: {question}
+        <div className="px-16 py-8 text-xl text-center text-white bg-rose-400 rounded-md max-md:px-5 max-md:max-w-full">
+            {question}
         </div>
     )
 }
