ba88afb74f33bd12be2f7902e7609c8a775d5808
aixion1506 <aixion1506@gmail.com>
2024-10-08 11:24:06 +0900

    Docs: 캘린더 주석 추가

--- src/components/calendar/R_Calendar.tsx
+++ src/components/calendar/R_Calendar.tsx
@@ -5,38 +5,39 @@ import interactionPlugin from '@fullcalendar/interaction'
 import { DateClickArg, EventClickArg, EventContentArg } from 'fullcalendar';
 
 export default function Calendar() {
-    const events = () => {
-      return[
-        { title: 'Event 1', date: '2024-10-07' },
-        { title: 'Event 2', date: '2024-10-08' },
-        { title: 'Event 3', start: new Date('2024-10-09'), end: new Date('2024-10-10')},
-      ]}
 
-      const allowEvents = events().map(event => event.date);
+  // 이벤트 리스트
+  const events = () => {
+    return[
+      { title: 'Event 1', date: '2024-10-07' },
+      { title: 'Event 2', date: '2024-10-08' },
+      { title: 'Event 3', start: new Date('2024-10-09'), end: new Date('2024-10-10')},
+    ]}
+  const allowEvents = events().map(event => event.date);
 
 
-      const handleDateClick = (info:DateClickArg) => {
-        if (allowEvents.includes(info.dateStr) ) {
-          alert('Date is allowed: ' + info.dateStr);
-        }
-      };
-      const handleEventClick = (info: EventClickArg) => {
-        alert('Event clicked: ' + info.event.title);
-      };
+  // 일정 클릭 알림
+  const handleDateClick = (info:DateClickArg) => {
+    if (allowEvents.includes(info.dateStr) ) {
+      alert('Date is allowed: ' + info.dateStr);
+    }
+  };
+  // 이벤트 클릭 알림
+  const handleEventClick = (info: EventClickArg) => {
+    alert('Event clicked: ' + info.event.title);
+  };
 
-    return (
-      <FullCalendar
-        plugins={[ dayGridPlugin, interactionPlugin ]}
-        initialView="dayGridMonth"
-        events={events()}
-        eventContent={renderEventContent}
-        dateClick={handleDateClick}
-        editable={true}
-        eventClick={handleEventClick}
-        // selectable={true}
-        // selectMirror={true}
-      />
-    )
+  return (
+    <FullCalendar
+      plugins={[ dayGridPlugin, interactionPlugin ]}
+      initialView="dayGridMonth"
+      events={events()}
+      eventContent={renderEventContent}
+      dateClick={handleDateClick}
+      editable={true}
+      eventClick={handleEventClick}
+    />
+  )
 }
 
   function renderEventContent(eventInfo: EventContentArg) {
