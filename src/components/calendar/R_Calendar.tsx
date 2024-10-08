3f6eb3e24a46199c9296cba97902e4f68d44c70c
aixion1506 <aixion1506@gmail.com>
2024-10-08 10:49:48 +0900

    Feat: 캘린더 이벤스 리스트 객체화

--- src/components/calendar/R_Calendar.tsx
+++ src/components/calendar/R_Calendar.tsx
@@ -4,15 +4,18 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import { EventContentArg } from 'fullcalendar';
 
 export default function Calendar() {
-    const events = [
-        { title: 'Event 1', date: '2023-10-07' },
-        { title: 'Event 2', date: '2023-10-08' }
-      ];
+    const events = () => {
+      return[
+        { title: 'Event 1', date: '2024-10-07' },
+        { title: 'Event 2', date: '2024-10-08' },
+        { title: 'Event 3', start: new Date('2024-10-09'), end: new Date('2024-10-10')},
+      ]}
+
     return (
       <FullCalendar
         plugins={[ dayGridPlugin ]}
         initialView="dayGridMonth"
-        events={events}
+        events={events()}
         eventContent={renderEventContent}
       />
     )
@@ -21,7 +24,7 @@ export default function Calendar() {
   function renderEventContent(eventInfo: EventContentArg) {
     return(
       <>
-        <b>{eventInfo.timeText}</b>
+        {/* <b>{eventInfo.timeText}</b> */}
         <i>{eventInfo.event.title}</i>
       </>
     )
