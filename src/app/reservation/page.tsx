61fbdbcc7df1444edb640f6f73546bcb848f1c6c
aixion1506 <aixion1506@gmail.com>
2024-10-08 16:13:29 +0900

    Rename: 폴더 라우팅명 변경

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -1,11 +1,9 @@
-import CalendarComponent from '@/components/calendar/Calendar';
-import Calendar from '@/components/calendar/R_Calendar';
+import Calendar from '@/components/calendar/Calendar';
 import React from 'react'
 
 const CaledarPage = () => {
   return (
     <div>
-        {/* <CalendarComponent /> */}
         <Calendar />
     </div>
   )
