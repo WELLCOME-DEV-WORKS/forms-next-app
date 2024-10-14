c6d88b9fb9de0ea7ff336f2b195a3f6f855b4ba9
nayounghye <yhna.dev@gmail.com>
2024-10-14 14:59:31 +0900

    Style: 푸터 구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,21 +42,7 @@ export default function RootLayout({
           >
             {children}
           </div>
-          <WellcomeFooter
-            text={
-              <>
-                Company Name : Well-come Visa Co., Ltd. <br />
-                Representative : Inhyuk Lee <br />
-                Address : 11th Floor, room 1113 and 1115, 78 Mapo-daero,
-                Mapo-gu, Seoul <br />
-                Business Registration Number : 359-86-02092 <br />
-                Email : info@well-come.biz <br />
-                Mail Order Business Registration Number : 2023-Seoul Mapo-3768{' '}
-                <br />
-                Customer Service : +82-70-8655-2888"
-              </>
-            }
-          />
+          <WellcomeFooter />
           <Toaster />
         </Providers>
       </body>
