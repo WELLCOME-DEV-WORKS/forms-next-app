2818af2f7c3eedc5e61e8e2dd3b603c50570f36c
aixion1506 <aixion1506@gmail.com>
2024-10-14 15:01:31 +0900

    Feat: userSurvey 예약 버튼 클릭시, 구글 간편 로그인 기능

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export const fetchCache = 'force-no-store';
 export default function Home() {
   return (
     <>
-      <Redirect />
+      {/* <Redirect /> */}
       <h1 className="font-bold text-3xl md:text-4xl">Landing Page</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* {feed} */}
--- src/components/userSurvey/NavigationButtons.tsx
+++ src/components/userSurvey/NavigationButtons.tsx
@@ -1,3 +1,9 @@
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
 interface NavigationButtonsProps {
     currentQuestionIndex: number;
     totalQuestions: number;
@@ -5,6 +11,32 @@ interface NavigationButtonsProps {
     onPrevious: () => void;
 }
 const NavigationButtons = ({ currentQuestionIndex, totalQuestions, onNext, onPrevious }: NavigationButtonsProps) => {
+    const router = useRouter();
+    const [session, setSession] = useState<Session | null>(null);
+    const [redirectToReservation, setRedirectToReservation] = useState(false);
+
+    const handleNext  = async () => {
+        const session = await getSession();
+        console.log('session =>', session);
+        
+        if (currentQuestionIndex === totalQuestions - 1) {
+            if (!session) {
+                setRedirectToReservation(true);
+                await router.push('/sign-in');
+                return ;
+            }
+            await router.push('/reservation');
+        } else {
+            onNext();
+        } 
+    }
+
+    useEffect(() => {
+        if (redirectToReservation && session) {
+            router.push('/reservation');
+        }
+    }, [session, redirectToReservation, router]);
+
     return (
         <div className="flex justify-center gap-5 self-center mt-11 max-w-full text-center">
             <div 
@@ -16,9 +48,9 @@ const NavigationButtons = ({ currentQuestionIndex, totalQuestions, onNext, onPre
             </div>
             <div 
                 className="px-10 py-3.5 text-rose-400 bg-white rounded border-2 border-rose-400 border-solid max-md:px-5"
-                onClick={onNext}
+                onClick={handleNext}
             >
-                {currentQuestionIndex === totalQuestions - 1 ? '제출' : '다음'}
+                {currentQuestionIndex === totalQuestions - 1 ? '예약' : '다음'}
             </div>
         </div>
     );
