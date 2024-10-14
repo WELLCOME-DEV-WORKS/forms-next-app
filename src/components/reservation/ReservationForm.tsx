79bc86f48141cef87a45ae64ad90fd381fb01adc
nayounghye <yhna.dev@gmail.com>
2024-10-14 16:05:55 +0900

    Style: 마진값 조절

--- src/components/reservation/ReservationForm.tsx
+++ src/components/reservation/ReservationForm.tsx
@@ -3,17 +3,21 @@ interface FormFieldProps {
   ans: string;
 }
 
-const FormField = ({ label, ans }: FormFieldProps)  => {
+const FormField = ({ label, ans }: FormFieldProps) => {
   return (
     <div className="relative mb-8 ">
-        <div className="rounded-lg flex items-center justify-between">
-            <div 
-            className="bg-wellcome-pink rounded-lg 
+      <div className="rounded-lg flex items-center justify-between">
+        <div
+          className="bg-wellcome-pink rounded-lg 
             p-4 w-1/3 z-10 text-center text-white text-xl
-             font-bold">{label}</div>
-            <div className="w-2/3 z-10 text-center justify-center text-wellcome-pink text-xl font-bold">
-            {ans}</div>
+             font-bold"
+        >
+          {label}
         </div>
+        <div className="w-2/3 z-10 text-center justify-center text-wellcome-pink text-xl font-bold">
+          {ans}
+        </div>
+      </div>
     </div>
     // <div className="flex flex-col mt-12 max-w-full text-black bg-white w-[349px] max-md:mt-10">
     //   <div className="px-9 py-4 bg-white max-md:px-5">{label}</div>
@@ -23,13 +27,13 @@ const FormField = ({ label, ans }: FormFieldProps)  => {
 
 const ReservationForm = () => {
   const fields = ['시술 내역', '가격', '예약 일정'];
-  const ans = ['울쎄라', '100만원', '24년 10월 26일']
+  const ans = ['울쎄라TEST', 'TEST만원', '24년 10월 26일'];
 
   return (
     <form className="flex flex-col  itmes-center  justify-">
       {fields.map((field, index) => (
-          <FormField key={index} label={field} ans={ans[index]} />
-        ))}
+        <FormField key={index} label={field} ans={ans[index]} />
+      ))}
     </form>
   );
 };
