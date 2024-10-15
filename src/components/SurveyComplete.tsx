9e1802eb7b88c4dc681fe72a1d63b9c80dd8cc2e
aixion1506 <aixion1506@gmail.com>
2024-10-15 16:23:44 +0900

    Feat: SurveyComplete 확인용 테스트

---
+++ src/components/SurveyComplete.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useStoreSurvey } from '@/store/Store';
+import React, { useEffect } from 'react'
+
+export const SurveyComplete = () => {
+    const surveyComplete = useStoreSurvey((state) => state.surveyComplete);
+    const setSurveyComplete = useStoreSurvey((state) => state.setSurveyComplete);
+    
+
+
+    const handleButtonClick = () => {
+        // 현재 상태에 따라 true/false를 토글합니다.
+        setSurveyComplete(!surveyComplete);
+    };
+    return (
+        <>
+        <hr />
+        <br />
+        <button onClick={handleButtonClick}>전환버튼</button>
+        <hr />
+            {surveyComplete ? (
+                <p>True</p>
+            ) : (
+                <p>False</p>
+            )}
+        </>
+  )
+}
