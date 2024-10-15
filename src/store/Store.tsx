ace605e599eb86608bbffd2651be7bbc4a0f717a
aixion1506 <aixion1506@gmail.com>
2024-10-15 15:17:44 +0900

    Feat: Zustand를 이용한 useStoreSurvey 생성

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect } from 'react';
 import { create } from 'zustand'
 
 interface StoreState {
@@ -8,7 +9,12 @@ interface StoreState {
 }
 
 // Zustand 스토어 생성
-export const useStore = create<StoreState>((set) => ({
-    surveyComplete: false,
-    setSurveyComplete: (complete) => set({ surveyComplete: complete }), // 상태 업데이트 함수
-}));
+export const useStoreSurvey = create<StoreState>((set) => ({
+        surveyComplete: false,
+        setSurveyComplete: (complete) => {
+            console.log('설문 완료 상태 업데이트:', complete);
+            set({ surveyComplete: complete }); // 상태 업데이트
+            localStorage.setItem('SurveyComplete', JSON.stringify(complete)); // 로컬 스토리지에 저장
+            console.log('로컬 스토리지에 저장:', complete);
+        },
+    }));
