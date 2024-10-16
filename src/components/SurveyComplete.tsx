055b9f70fe26c9f0c611e4a22ed98e76a0daabf1
aixion1506 <aixion1506@gmail.com>
2024-10-16 14:23:07 +0900

    Test: 컴포넌트 마운트시, SurveyComplete 값을 추출

--- src/components/SurveyComplete.tsx
+++ src/components/SurveyComplete.tsx
@@ -7,7 +7,10 @@ export const SurveyComplete = () => {
     const surveyComplete = useStoreSurvey((state) => state.surveyComplete);
     const setSurveyComplete = useStoreSurvey((state) => state.setSurveyComplete);
     
-
+    useEffect(() => {
+        const storedSurveyComplete = JSON.parse(localStorage.getItem('SurveyComplete') || 'false');
+        setSurveyComplete(storedSurveyComplete);
+    }, [setSurveyComplete]);
 
     const handleButtonClick = () => {
         // 현재 상태에 따라 true/false를 토글합니다.
