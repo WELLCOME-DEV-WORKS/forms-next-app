d1d59c24127c1e97fbf7c26ab4357bfafe010a29
aixion1506 <aixion1506@gmail.com>
2024-10-16 17:40:53 +0900

    Feat: Zustand Devtools

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,20 +1,21 @@
 'use client'
-import { useEffect } from 'react';
-import { create } from 'zustand'
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 interface StoreState {
     surveyComplete: boolean;
-    // eslint-disable-next-line no-unused-vars
     setSurveyComplete: (complete: boolean) => void;
 }
 
 // Zustand 스토어 생성
-export const useStoreSurvey = create<StoreState>((set) => ({
-        surveyComplete: false,
-        setSurveyComplete: (complete) => {
-            console.log('설문 완료 상태 업데이트:', complete);
-            set({ surveyComplete: complete }); // 상태 업데이트
-            localStorage.setItem('SurveyComplete', JSON.stringify(complete)); // 로컬 스토리지에 저장
-            console.log('로컬 스토리지에 저장:', complete);
-        },
-    }));
+export const useStoreSurvey = create<StoreState>()(
+  devtools((set) => ({
+    surveyComplete: false,
+    setSurveyComplete: (complete) => {
+      console.log('설문 완료 상태 업데이트:', complete);
+      set({ surveyComplete: complete }); // 상태 업데이트
+      localStorage.setItem('SurveyComplete', JSON.stringify(complete)); // 로컬 스토리지에 저장
+      console.log('로컬 스토리지에 저장:', complete);
+    },
+  }), { name: "SurveyStore" }) // 스토어 이름 설정
+);
