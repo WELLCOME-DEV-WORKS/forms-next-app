d7c750ff38414756161455f0e2a840e2c1e8ac83
aixion1506 <aixion1506@gmail.com>
2024-10-18 17:37:16 +0900

    Feat: 설문 결과 스토어 생성 및 설문 완료 스토어 이름 변경

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -2,13 +2,13 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-interface StoreState {
+interface SurveyCompleteState {
   surveyComplete: boolean;
   setSurveyComplete: (complete: boolean) => void;
 }
 
 // Zustand 스토어 생성
-export const useStoreSurvey = create<StoreState>()(
+export const useSurveyCompleteStore = create<SurveyCompleteState>()(
   devtools(
     (set) => ({
       surveyComplete: false,
@@ -18,6 +18,62 @@ export const useStoreSurvey = create<StoreState>()(
         console.log('로컬 스토리지에 저장:', complete);
       },
     }),
-    { name: 'SurveyStore' }
+    { name: 'SurveyCompleteStore' }
   ) // 스토어 이름 설정
+);
+
+interface SurveyResultsState {
+  treatmentPurpose: string | undefined;
+  treatmentMethod: string | undefined;
+  price: string | undefined;
+  injectionArea: string | undefined;
+  sideEffects: string | undefined;
+  setSurveyResults: (answers: string[]) => void; // 결과를 설정하는 메서드
+}
+export const useSurveyResultsStore = create<SurveyResultsState>()(
+  devtools(
+    (set) => ({
+      treatmentPurpose: undefined,
+      treatmentMethod: undefined,
+      injectionArea: undefined,
+      sideEffects: undefined,
+      price: undefined,
+      setSurveyResults: (answers) => {
+        const treatmentPurpose = answers[0] || '기본값'; // 기본값 설정
+        const treatmentMethod =
+          answers[1] || answers[2] || answers[3] || '기타'; // 기본값 설정
+        const injectionArea = answers[4] || '기타'; // 기본값 설정
+        const sideEffects = answers[5] || '없음'; // 기본값 설정
+        const price = answers[6] || '미정'; // 기본값 설정
+
+        set({
+          treatmentPurpose: treatmentPurpose,
+          treatmentMethod: treatmentMethod,
+          injectionArea: injectionArea,
+          sideEffects: sideEffects,
+          price: price,
+        });
+
+        // 결과를 로컬 스토리지에 키-값 형태로 저장
+        const surveyResults = {
+          treatmentPurpose,
+          treatmentMethod,
+          injectionArea,
+          sideEffects,
+          price,
+        };
+        localStorage.setItem('SurveyResults', JSON.stringify(surveyResults)); // 결과를 로컬 스토리지에 저장
+        console.log('설문 결과 저장:', surveyResults);
+      },
+      clearSurveyResults: () =>
+        set({
+          treatmentPurpose: undefined,
+          treatmentMethod: undefined,
+          price: undefined,
+          injectionArea: undefined,
+          sideEffects: undefined,
+        }),
+    }),
+    { name: 'SurveyStore' }
+  )
 );
