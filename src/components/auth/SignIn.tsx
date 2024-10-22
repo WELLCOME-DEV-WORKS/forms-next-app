93c3d662b3b6b9712bb88fb33e81a4e1e5c1e773
nayounghye <yhna.dev@gmail.com>
2024-10-22 14:24:29 +0900

    Fix: 로그인 메시지 변경

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,23 +1,27 @@
-import { FC } from "react";
-import { Icons } from "../ui/details/Icons";
-import Link from "next/link";
-import UserAuthForm from "./UserAuthForm";
+import { FC } from 'react';
+import { Icons } from '../ui/details/Icons';
+import Link from 'next/link';
+import UserAuthForm from './UserAuthForm';
 
 const SignIn: FC = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-6 w-6" />
-        <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
-        <p className="text-sm max-w-xs mx-auto">
-          By continuing, you are setting up a community account and agree to our
-          User Agreement and Privacy Policy.
-        </p>
+        {/* <Icons.logo className="mx-auto h-6 w-6" /> */}
+        <h1 className="text-2xl font-semibold tracking-tight">
+          추천결과를 확인하려면
+          <br />
+          로그인이 필요합니다.
+        </h1>
+        {/* <p className="text-sm max-w-xs mx-auto text-left">
+          <br />
+          로그인 후 추천 결과를 확인하고 예약을 진행할 수 있습니다.
+        </p> */}
       </div>
       <UserAuthForm />
 
       <p className="px-8 text-center text-sm text-muted-foreground">
-        New to community?{" "}
+        New to community?{' '}
         <Link
           href="/sign-up"
           className="hover:text-brand text-sm underline underline-offset-4"
