628c76794301516b792758d9a7b4459c4544d041
nayounghye <yhna.dev@gmail.com>
2024-10-25 11:42:56 +0900

    Fix: 주석 추가

--- src/components/recommendation/RecLogic.tsx
+++ src/components/recommendation/RecLogic.tsx
@@ -2,11 +2,11 @@
 import RecommendationList from '@/components/recFlow/RecommendationList';
 import { PricesList } from '@/components/recFlow/PricesList';
 
-// 추천 시술을 가져오는 로직
+// 추천 시술 로직
 export const getRecResults = (
-  treatmentPurpose: string,
-  treatmentMethod: string,
-  price: string
+  treatmentPurpose: string, // 고민 사항
+  treatmentMethod: string, // 시술 방법
+  price: string // 가격
 ) => {
   const recommended = RecommendationList().find(
     (rec) =>
