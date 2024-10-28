9c42a003eff0cae5a24e115b5ef7f3a14296c4be
nayounghye <yhna.dev@gmail.com>
2024-10-28 09:17:01 +0900

    Style: 레이아웃 변경

--- src/components/recommendation/RecForm.tsx
+++ src/components/recommendation/RecForm.tsx
@@ -11,8 +11,8 @@ interface FormFieldProps {
 const FormField = ({ label, ans, RecButton }: FormFieldProps) => {
   return (
     <div className="flex flex-col mb-8 w-full h-full items-center justify-center max-lg:flex-row">
-      <div className="px-11 py-2 flex flex-col items-center justify-center w-full h-full">
-        <div className="bg-wellcome-pink rounded-lg p-4 w-full z-10 text-center text-white text-xl font-bold">
+      <div className="flex flex-col px-11 py-2 items-center justify-center w-full h-full">
+        <div className=" bg-wellcome-pink rounded-lg p-4 w-full z-10 text-center text-white text-xl font-bold">
           {label}
         </div>
         <div className="w-full px-16 py-9 mt-11 rounded-2xl text-left justify-center items-center text-wellcome-pink text-xl font-bold border-2 border-rose-400 border-dashed leading-loose max-md:px-5 max-md:mt-10 max-md:max-w-full" style={{ whiteSpace: 'pre-line' }}>
@@ -28,7 +28,7 @@ const FormField = ({ label, ans, RecButton }: FormFieldProps) => {
                 <div key={index} className="mb-2">
                   <button
                     type="button"
-                    className="bg-wellcome-pink text-white font-bold py-2 px-4 rounded"
+                    className="flex flex-row bg-wellcome-pink text-white font-bold py-2 px-4 rounded-lg"
                     onClick={() => alert(`Clicked on: ${item}`)} 
                   >
                     {item} {/* 버튼형식 */}
@@ -64,16 +64,13 @@ const RecForm = () => {
   // 비용 계산 로직
   const calculatePriceDetails = (methods: string) => {
     if (!methods) return []; // 값이 없으면 빈 배열 반환
-    const methodList = methods.split(', '); // 쉼표로 구분하여 배열로 변환
-    return methodList.map(method => {
-      // 추천 결과가 '조건에 부합하는 상품이 없습니다.'인 경우
-      if (method === '조건에 부합하는 상품이 없습니다.') {
-        return method; // 가격 정보 없이 그대로 반환
-      }
-      return `${method}: ${PricesList[method] || '정보 없음'}`; // 가격 정보 포함
-    });
+    return methods.split(', ').map(method => 
+      method === '조건에 부합하는 상품이 없습니다.' 
+        ? method // 가격 정보 없이 그대로 반환 
+        : `${method}: ${PricesList[method] || '정보 없음'}` // 가격 정보 포함
+    );
   };
-
+  
   const fields = ['추천 시술', '그 외 시술'];
   const ans = [
     calculatePriceDetails(recommendedMethod || ''),
@@ -81,7 +78,7 @@ const RecForm = () => {
   ];
 
   return (
-    <form className="flex justify-center w-[85%] ">
+    <form className="flex flex-wrap justify-center w-[85%] ">
       {fields.map((field, index) => (
         <FormField key={index} label={field} ans={ans[index]} RecButton={index === 0} />
       ))}
