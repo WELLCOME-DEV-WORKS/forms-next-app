4c44cb5fb96161000652b7ab547a2f61c48870c3
aixion1506 <aixion1506@gmail.com>
2024-11-01 14:42:39 +0900

    Refactor: 동일 스타일, 스타일 객체 처리

--- src/components/recommendation/RecommendationBtn.tsx
+++ src/components/recommendation/RecommendationBtn.tsx
@@ -13,17 +13,18 @@ interface ReservationBtnProps {
   }: ReservationBtnProps) => {
     const { handleReservationClick } = RecommendationBtnHandler(recommendedTreatment);
   
+    const Style = 'px-11 py-3.5 my-8 text-2xl font-bold text-center text-white bg-wellcome-pink rounded-[44px] shadow-lg hover:text-[#EA708A] hover:bg-[#FEE4E3] transition-colors duration-300'
+
     return noRecommendation ? (
       <Link href="/userSurvey" passHref>
-        <button className="px-11 py-3.5 my-8 text-2xl font-bold text-center text-white bg-wellcome-pink rounded-[44px] shadow-lg hover:text-[#EA708A] hover:bg-[#FEE4E3] transition-colors duration-300">
+        <button className={Style}>
           다시 추천받기
         </button>
       </Link>
     ) : (
       <button
         onClick={handleReservationClick}
-        className="px-11 py-3.5 my-8 text-2xl font-bold text-center text-white bg-wellcome-pink rounded-[44px] shadow-lg hover:text-[#EA708A] hover:bg-[#FEE4E3] transition-colors duration-300
-        max-md:text-xl max-md:my-7 max-sm:text-base max-sm:my-4"
+        className={`${Style} max-md:text-xl max-md:my-7 max-sm:text-base max-sm:my-4`}
       >
         예약일정 보러가기
       </button>
