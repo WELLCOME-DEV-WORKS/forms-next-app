74bc255166eda66779ac62a2ae9549c1e8613f3d
nayounghye <yhna.dev@gmail.com>
2024-11-01 13:59:58 +0900

    Style: userSurvey 질문 배경색상 변경

--- src/components/userSurvey/SurveyQuestion.tsx
+++ src/components/userSurvey/SurveyQuestion.tsx
@@ -1,13 +1,13 @@
-interface SurveyQuestionProps  {
-    question: string
+interface SurveyQuestionProps {
+  question: string;
 }
 
-const SurveyQuestion = ({question}: SurveyQuestionProps ) => {
-    return (
-        <div className="px-16 py-8 text-xl text-center text-white bg-rose-400 rounded-md max-md:px-5 max-md:max-w-full">
-            {question}
-        </div>
-    )
-}
+const SurveyQuestion = ({ question }: SurveyQuestionProps) => {
+  return (
+    <div className="px-16 py-8 text-xl text-center text-white bg-wellcome-pink rounded-md max-md:px-5 max-md:max-w-full">
+      {question}
+    </div>
+  );
+};
 
 export default SurveyQuestion;
